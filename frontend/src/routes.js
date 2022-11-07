e597b579a727483d237445f468166b5708a34e2d
Wiliender <wiliendersilva@gmail.com>
2022-11-06 23:12:03 -0300

    Feat: Create /register /profile and /incidents/new

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,12 +6,19 @@ import {
   } from "react-router-dom";
 
 import Logon from './pages/Logon';
+import Register from './pages/Register'
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Rotas() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Logon />} />
+                <Route path="/register" element={<Register />} />
+
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/incidents/new" element={<NewIncident />} />
             </Routes>
         </BrowserRouter>
     );
